fdfa21340955acc8d9556b2b111b14dfa6ae5a67
bwraab <blake@n4bwr.com>
2025-07-06 13:27:55 -0400

    Fix CORS configuration to allow requests from n4bwr.com

--- app.js
+++ app.js
@@ -29,7 +29,12 @@ app.use(helmet({
   }
 }));
 app.use(compression());
-app.use(cors());
+app.use(cors({
+  origin: ['https://n4bwr.com', 'http://localhost:3000', 'https://balloon-tracker-api.onrender.com'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json({ limit: '10mb' }));
 
 // Serve static files from the React build directory
